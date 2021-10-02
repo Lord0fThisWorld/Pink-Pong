6da36b3f0082aa073470fcf397155187315dfa26
Tomasz_Zdeb <t.z.t.968@gmail.com>
2021-10-02 10:00:00 +0200

    Simplified comments in Game->Gameplay->GameplayScreen.cs

--- PaddleHit/Gameplay/GameplayScreen.cs
+++ PaddleHit/Gameplay/GameplayScreen.cs
@@ -10,30 +10,22 @@ namespace Game
     {
         #region variables
         ScoreCounter scoreCounter;
-        // variables that holds dimensions to the time when board is created (they are passed to boards creator)
         int width, height;
-        // board variable
         Board board;
-        // paddle variables
         Paddle paddle1, paddle2;
         Ball ball;
         int ballSpeed;
         #endregion
+
         public GameplayScreen(int width, int height)
         {
-            // writes (dimensions passed to PingPong constructor) to PingPong variables
             this.width = width;
             this.height = height;
-            //creates board instance
             board = new Board(height, width);
-            //creates new ball instance
             ball = new Ball(width / 2, height / 2, height, width);
-            // necessary for calculating time span for time dependant events
             startupDate = DateTime.Now;
         }
-        /// <summary>
-        /// initial gameplay setup
-        /// </summary>
+
         public void Setup()
         {
             paddle1 = new Paddle(2, height, width, 6);
@@ -46,7 +38,7 @@ namespace Game
             scoreCounter = new ScoreCounter();
         }
         /// <summary>
-        /// Main loop responsible for the gameplay sentence (reading input, coputing and printing output in the right sequence)
+        /// Main loop responsible for the whole setup and gameplay
         /// </summary>
         public int Run(int temp)
         {
@@ -66,14 +58,11 @@ namespace Game
             paddle2.Write();
             ball.Write();
             #endregion
-            //GAME LOOP - runs until ball missed the paddle - end of a round
+            // Gameplay loop
             while (ball.X != 1 && ball.X != width - 1)
             {
-                // clock, responsible for time dependant events
                 mainClock = DateTime.Now - startupDate;
-                // reads input key
                 Input();
-                //depending on key value performs a specific action
                 switch (consoleKey)
                 {
                     case ConsoleKey.W:
@@ -93,21 +82,16 @@ namespace Game
                         paddle2.Down();
                         break;
                 }
-                // resets consoleKey variable value so pressing W or S does not make it go up the way up or down but only makes one move
-                consoleKey = ConsoleKey.A;
+                consoleKey = ConsoleKey.A; //resets the key so it's not pressed permanently
                 if (mainClock.TotalMilliseconds > ballSpeed)
                 {
                     startupDate = DateTime.Now;
-                    // method responsible for propper ball movement
                     if (ball.Physics(paddle1, paddle2, mainClock))
                     {
                         scoreCounter.score++;
                     }
-                    // method responsible for printing the ball graphical interpretations
                     ball.Write();
                 }
-                
-                // prints score counter in the middle of the screen
                 scoreCounter.Write(width);
                 //Console.SetCursorPosition(width / 2 - 3, 3);
                 //Console.Write("clock: " + (mainClock.Milliseconds == 999));
