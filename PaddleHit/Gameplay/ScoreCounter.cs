ce83fab9f3e345770438c5f6b7f8a6bc37b3cde2
Tomasz Zdeb <t.z.t.968@gmail.com>
2021-10-08 14:39:43 +0200

    Changed ScoreCounter constructor to lambda syntax

--- PaddleHit/Gameplay/ScoreCounter.cs
+++ PaddleHit/Gameplay/ScoreCounter.cs
@@ -7,16 +7,15 @@ namespace Game
     class ScoreCounter
     {
         // Counts paddle hits
-        public int score { get; set; }
-        public ScoreCounter()
-        {
-            score = 0;
-        }
+        public int Score { get; set; }
+
+        public ScoreCounter() => Score = 0;
+
         public void Write(int width)
         {
             Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.SetCursorPosition(width/2 -3, 1);
-            Console.Write("Score: "+ score);
+            Console.Write("Score: "+ Score);
         }
     }
 }
