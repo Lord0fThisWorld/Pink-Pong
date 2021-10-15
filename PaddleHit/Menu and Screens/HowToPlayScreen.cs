87ccde251bc0925b1443c40ea12b3a7e4f32272d
Tomasz Zdeb <t.z.t.968@gmail.com>
2021-10-15 15:41:52 +0200

    Exported HowToPlayScreen routines into methods Code contained in three regions was moved to separate methods for clarity and maintanability increase.

--- PaddleHit/Menu and Screens/HowToPlayScreen.cs
+++ PaddleHit/Menu and Screens/HowToPlayScreen.cs
@@ -21,59 +21,64 @@ namespace Game
 
                 OuterFrameDraw(width, height, '▓');
                 InnerFrameDraw(xStart, yStart, '▒');
+                DrawContent(xStart,yStart);
+                DrawPressEnter(xStart, yStart, mainClock);
+                DrawSideLines(xStart, yStart);
 
-                #region HELP CONTENT
-                Console.SetCursorPosition(xStart + 5,yStart + 2);
-                Console.Write("1) Use W/S or up/down arrow keys to move the");
-                Console.SetCursorPosition(xStart + 5,yStart + 3);
-                Console.Write("   paddles, be aware that game supports only");
-                Console.SetCursorPosition(xStart + 5, yStart + 4);
-                Console.Write("   clicking, you CAN NOT hold the key");
-                Console.SetCursorPosition(xStart + 5, yStart + 6);
-                Console.Write("2) Try to hit the ball with the paddle or You");
-                Console.SetCursorPosition(xStart + 5, yStart + 7);
-                Console.Write("   will lose the game if You miss");
-                Console.SetCursorPosition(xStart + 5, yStart + 9);
-                Console.Write("3) Central hit grants You straight shoot, that");
-                Console.SetCursorPosition(xStart + 5, yStart + 10);
-                Console.Write("   means the ball will move perpendicular to");
-                Console.SetCursorPosition(xStart + 5, yStart + 11);
-                Console.Write("   the paddles");
-                Console.SetCursorPosition(xStart + 5, yStart + 13);
-                Console.Write("4) Use setting to adjust gameplay to your needs");
-                #endregion
-
-                #region Press ENTER TEXT
-                if (((int)mainClock.TotalSeconds) % 2 == 0)
-                {
-                    Console.SetCursorPosition(xStart + 16,yStart + 15);
-                    Console.Write("Press ENTER to proceed... ");
-                }
-                else
-                {
-                    Console.SetCursorPosition(xStart + 16,yStart + 15);
-                    Console.Write("                          ");
-                }
-                #endregion
-
-                #region side lines
-                // left from top to bottom
-                for (int i = yStart + 2; i < yStart + 15; i++)
-                {
-                    Console.SetCursorPosition(xStart + 3, i);
-                    Console.Write("│");
-                }
-                // right from top to bottom
-                for (int i = yStart + 2; i < yStart + 15; i++)
-                {
-                    Console.SetCursorPosition(xStart + 53, i);
-                    Console.Write("│");
-                }
-                #endregion
                 CheckIfKeyIsPressed();
             }
             Console.Clear();
             return ((temp / 10) * 10);
         }
+        private void DrawContent(int xStart, int yStart)
+        {
+            Console.SetCursorPosition(xStart + 5, yStart + 2);
+            Console.Write("1) Use W/S or up/down arrow keys to move the");
+            Console.SetCursorPosition(xStart + 5, yStart + 3);
+            Console.Write("   paddles, be aware that game supports only");
+            Console.SetCursorPosition(xStart + 5, yStart + 4);
+            Console.Write("   clicking, you CAN NOT hold the key");
+            Console.SetCursorPosition(xStart + 5, yStart + 6);
+            Console.Write("2) Try to hit the ball with the paddle or You");
+            Console.SetCursorPosition(xStart + 5, yStart + 7);
+            Console.Write("   will lose the game if You miss");
+            Console.SetCursorPosition(xStart + 5, yStart + 9);
+            Console.Write("3) Central hit grants You straight shoot, that");
+            Console.SetCursorPosition(xStart + 5, yStart + 10);
+            Console.Write("   means the ball will move perpendicular to");
+            Console.SetCursorPosition(xStart + 5, yStart + 11);
+            Console.Write("   the paddles");
+            Console.SetCursorPosition(xStart + 5, yStart + 13);
+            Console.Write("4) Use setting to adjust gameplay to your needs");
+        }
+        private void DrawPressEnter(int xStart, int yStart, TimeSpan mainClock)
+        {
+            if (((int)mainClock.TotalSeconds) % 2 == 0)
+            {
+                Console.SetCursorPosition(xStart + 16, yStart + 15);
+                Console.Write("Press ENTER to proceed... ");
+            }
+            else
+            {
+                Console.SetCursorPosition(xStart + 16, yStart + 15);
+                Console.Write("                          ");
+            }
+        }
+        private void DrawSideLines(int xStart, int yStart)
+        {
+            // left from top to bottom
+            for (int i = yStart + 2; i < yStart + 15; i++)
+            {
+                Console.SetCursorPosition(xStart + 3, i);
+                Console.Write("│");
+            }
+            // right from top to bottom
+            for (int i = yStart + 2; i < yStart + 15; i++)
+            {
+                Console.SetCursorPosition(xStart + 53, i);
+                Console.Write("│");
+            }
+        }
     }
+
 }
