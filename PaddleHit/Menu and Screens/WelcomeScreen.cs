09068a677479e48a9861dff00e8ba9a051ca15ee
Tomasz Zdeb <t.z.t.968@gmail.com>
2021-10-16 15:18:42 +0200

    Cleaned WelcomeScreen code Deleted unnecessary comments and spaces

--- PaddleHit/Menu and Screens/WelcomeScreen.cs
+++ PaddleHit/Menu and Screens/WelcomeScreen.cs
@@ -5,36 +5,30 @@ namespace Game
 {
     class WelcomeScreen : ScreenBase
     {
-        //Startup dimensions
+
         int initialWidth = 61;
         int initialHeight = 20;
-        /// <summary>
-        /// Sets propper window size and gets creation DateTime
-        /// </summary>
+
         public WelcomeScreen()
         {
-            // when app starts, the initial screen will be the same all the time, later in menu size will be changeable
             Console.SetWindowSize(63, 22);
             Console.SetBufferSize(63, 22);
             startupDate = DateTime.Now;
         }
-        /// <summary>
-        /// Actual screen (requires settup provided by constructor)
-        /// </summary>
+
         public void Screen()
         {
             OuterFrameDraw(initialWidth, initialHeight, '▓');
             InnerFrameDraw(3, 2, '▒');
             while (consoleKey != ConsoleKey.Enter)
             {
-                // difference between startup date (set in constructor) and now, gives timespan of how long the screen is running
-                // thanks to that some time dependent events cann occur like, blinking and color change
+
                 mainClock = DateTime.Now - startupDate;
-                // procedurally generated word, drawed by squares (single characters)
+
                 WordDraw(8, 5, "welcome");
                 Console.SetCursorPosition(23, 12);
                 Console.Write("To ");
-                //Some time dependent text
+
                 #region welcome message
                 if ((int)mainClock.TotalSeconds > 5 && ((((int)mainClock.TotalSeconds) % 10 == 0) ||
                     (((int)mainClock.TotalSeconds - 1) % 10 == 0) ||
@@ -66,16 +60,16 @@ namespace Game
                     Console.Write("                          ");
                 }
                 #endregion
-                // prevents blinking of items with high refresh rate
-                Thread.Sleep(1);
-                // helps to check if time dependent events occur right
-                //#region clock display (for development)
+                
+                Thread.Sleep(1); // prevents blinking of items due to high refresh rate
+
+                #region DEVELOPER_CLOCK
                 //Console.SetCursorPosition(24, 14);
                 //Console.Write("                   ");
                 //Console.SetCursorPosition(24, 14);
                 //Console.Write("Main Clock: " + (int)mainClock.TotalSeconds);
-                //#endregion
-                // listens for user input
+                #endregion
+
                 CheckIfKeyIsPressed();
             }
             Console.Clear();
