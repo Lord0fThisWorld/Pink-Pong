3a8145876e8ab25b51485c09f1182c739714e72a
Tomasz Zdeb <t.z.t.968@gmail.com>
2021-10-16 15:22:08 +0200

    exported content display to separate method in WelcomeScreen

--- PaddleHit/Menu and Screens/WelcomeScreen.cs
+++ PaddleHit/Menu and Screens/WelcomeScreen.cs
@@ -25,41 +25,7 @@ namespace Game
 
                 mainClock = DateTime.Now - startupDate;
 
-                WordDraw(8, 5, "welcome");
-                Console.SetCursorPosition(23, 12);
-                Console.Write("To ");
-
-                #region welcome message
-                if ((int)mainClock.TotalSeconds > 5 && ((((int)mainClock.TotalSeconds) % 10 == 0) ||
-                    (((int)mainClock.TotalSeconds - 1) % 10 == 0) ||
-                    (((int)mainClock.TotalSeconds - 2) % 10 == 0) ||
-                    (((int)mainClock.TotalSeconds - 3) % 10 == 0)))
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.Write("PaddleHit 2020");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    Console.SetCursorPosition(26, 12);
-                    Console.Write("PaddleHit 2020");
-                }
-                #endregion
-                Console.SetCursorPosition(21, 14);
-                Console.Write("A Console Arcade Game");
-                #region press enter to proceed
-                // 2 seconds of display 1 seccond of blank space
-                if ((((int)mainClock.TotalSeconds) % 3 == 0) || (((int)mainClock.TotalSeconds) % 3 == 1))
-                {
-                    Console.SetCursorPosition(20, 16);
-                    Console.Write("Press ENTER to proceed... ");
-                }
-                else
-                {
-                    Console.SetCursorPosition(20, 16);
-                    Console.Write("                          ");
-                }
-                #endregion
+                DisplayContent(mainClock);
                 
                 Thread.Sleep(1); // prevents blinking of items due to high refresh rate
 
@@ -76,5 +42,45 @@ namespace Game
             consoleKey = ConsoleKey.A;
         }
 
+        private void DisplayContent(TimeSpan mainClock)
+        {
+            WordDraw(8, 5, "welcome");
+            Console.SetCursorPosition(23, 12);
+            Console.Write("To ");
+
+            #region welcome message
+            if ((int)mainClock.TotalSeconds > 5 && ((((int)mainClock.TotalSeconds) % 10 == 0) ||
+                (((int)mainClock.TotalSeconds - 1) % 10 == 0) ||
+                (((int)mainClock.TotalSeconds - 2) % 10 == 0) ||
+                (((int)mainClock.TotalSeconds - 3) % 10 == 0)))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("PaddleHit 2020");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.SetCursorPosition(26, 12);
+                Console.Write("PaddleHit 2020");
+            }
+            #endregion
+
+            Console.SetCursorPosition(21, 14);
+            Console.Write("A Console Arcade Game");
+
+            #region press enter to proceed
+            // 2 seconds of display 1 seccond of blank space
+            if ((((int)mainClock.TotalSeconds) % 3 == 0) || (((int)mainClock.TotalSeconds) % 3 == 1))
+            {
+                Console.SetCursorPosition(20, 16);
+                Console.Write("Press ENTER to proceed... ");
+            }
+            else
+            {
+                Console.SetCursorPosition(20, 16);
+                Console.Write("                          ");
+            }
+            #endregion
+        }
     }
 }
